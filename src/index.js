fc6ab3d74648c4ff298ed179c71a64ecaa144e31
Robert Nadilo <robert.nadilo@kireygroup.com>
2020-01-13 14:34:16 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import getVisibleExpenses from './selectors/expenses'
+import { addExpense } from './actions/expenses'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+store.dispatch(addExpense({ description:'Water bill', amount: 4500, createdAt: 0 }))
+store.dispatch(addExpense({ description:'Gas bill', note: 'Petrol', amount: 0, createdAt: 1000 }))
+store.dispatch(addExpense({ description:'Rent', amount: 109500, createdAt: 0 }))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
